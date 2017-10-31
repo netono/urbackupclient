a382b924189bbfc60dbfa50e2b8fe42a24de0f3c
Jens Larsson <jens@netono.se>
2017-11-01 00:01:16 +0100

    19

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 ﻿FROM ubuntu:14.04
 
 RUN mkdir /backup
+RUN apt-get update && apt-get install -y wget
 
 COPY start.sh /opt/start.sh
 RUN chmod +x /opt/start.sh
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt-get update && apt-get install -y wget
 wget "http://hndl.urbackup.org/Client/${urbversion}/UrBackup%20Client%20Linux%20${urbversion}.sh" -O urbtemp
 yes 4 | sh urbtemp
 rm urbtemp
