d41854def79b0b98a97c8717535de330d835c7c8
Jens Larsson <jens@netono.se>
2017-10-31 22:15:59 +0100

    12

--- files/start.sh
+++ files/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+wget "http://hndl.urbackup.org/Client/2.0.31/UrBackup%20Client%20Linux%202.0.31.sh" -O urbtemp
+echo yes 4 | bash urbtemp
+rm urbtemp
+
 urbackupclientbackend -d
 urbackupclientctl add-backupdir -d /backup
