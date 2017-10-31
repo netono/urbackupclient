744e72ce24170c455803c82288408baeb4d8fbd8
Jens Larsson <jens@netono.se>
2017-11-01 00:27:40 +0100

    20

--- start.sh
+++ start.sh
@@ -2,6 +2,3 @@
 wget "http://hndl.urbackup.org/Client/${urbversion}/UrBackup%20Client%20Linux%20${urbversion}.sh" -O urbtemp
 yes 4 | sh urbtemp
 rm urbtemp
-service urbackupclientbackend restart
-
-urbackupclientctl add-backupdir -d /backup
