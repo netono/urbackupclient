c6dcb4c9da28dd4312618b78ffc75c1c1afa7dd4
Jens Larsson <jens@netono.se>
2017-11-02 22:22:56 +0100

    27

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ RUN mkdir /backup
 RUN apt-get update && apt-get install -y wget
 
 COPY start.sh /opt/start.sh
+COPY urbackupclient /opt/urbackupclient
 RUN chmod +x /opt/start.sh
--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
-﻿#!/bin/bash
+#!/bin/bash
 wget "http://hndl.urbackup.org/Client/${urbversion}/UrBackup%20Client%20Linux%20${urbversion}.sh" -O urbtemp
 
 yes 4 | sh urbtemp
 
 kill -9 $(pidof urbackupclientbackend)
+rm -f /etc/init.d/urbackupclientbackend
+/bin/cp -rf /opt/urbackupclient /etc/default
 
 urbackupclientbackend
